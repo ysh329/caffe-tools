2054135e139c3eb606cc3604114a41639523e96f
Shuai Yuan <ysh329@users.noreply.github.com>
2018-05-16 11:11:32 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+/opt/caffe/build/examples/cpp_classification/classification \
+/home/yuanshuai/code/inferx_model/squeezenet/squeezenetv11.prototxt \
+/home/yuanshuai/code/inferx_model/squeezenet/squeezenetv11.caffemodel \
+/opt/caffe/data/ilsvrc12/imagenet_mean.binaryproto \
+/opt/caffe/data/ilsvrc12/synset_words.txt \
+/home/yuanshuai/code/inferx_model/squeezenet/cat_perfxlab.jpg
